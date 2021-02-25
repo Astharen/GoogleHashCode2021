353ce59e8c205c0303877b0b99c6dd35bac038d3
Astharen <64799996+Astharen@users.noreply.github.com>
2021-02-25 20:16:19 +0100

    Add files via upload

--- output.py
+++ output.py
@@ -1,13 +1,12 @@
 def output_file(intersect_list, dict_dict_streets, dict_order_streets):
-
+    initialize_output()
     first_line = len(intersect_list)
     write_line(first_line)
 
     for intersect_name in intersect_list:
         dict_streets = dict_dict_streets[intersect_name]
         order_streets = dict_order_streets[intersect_name]
-        intersect_id = get_intersect_id(intersect_list, intersect_name)
-        write_each_intersect(intersect_name, dict_streets, order_streets, intersect_id)
+        write_each_intersect(intersect_name, dict_streets, order_streets, intersect_name)
 
 
 def write_each_intersect(intersect_name, dict_streets, order_streets, intersect_id):
@@ -22,16 +21,17 @@ def write_each_intersect(intersect_name, dict_streets, order_streets, intersect_
         write_line(var)
 
 
-def get_intersect_id(intersect_names, intersect_name):
-    return intersect_names.index(intersect_name)
-
-
 def write_line(var):
     with open('output.txt', 'a') as file:
         file.write(f'{var}\n')
 
 
-intersect_list = ['a', 'b']
-dict_dict_streets = {'a':{'ab':3, 'ba':4, 'ca':3}, 'b':{'ab':3, 'ba':4, 'ca':3}}
-dict_order_streets = {'a':['ab', 'ca', 'ba'], 'b':['ab', 'ca', 'ba']}
+def initialize_output():
+    with open('output.txt', 'w') as file:
+        file.write('')
+
+
+intersect_list = [0, 1]
+dict_dict_streets = {0:{'ab':3, 'ba':4, 'ca':3}, 1:{'ab':3, 'ba':4, 'ca':3}}
+dict_order_streets = {0:['ab', 'ca', 'ba'], 1:['ab', 'ca', 'ba']}
 output_file(intersect_list, dict_dict_streets, dict_order_streets)
